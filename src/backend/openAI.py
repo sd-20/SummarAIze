9c857c25f2eefb388f0adcc2394f489aafc8471c
andreaz3 <andrea60616@gmail.com>
2023-12-05 18:51:40 -0600

    added OpenAI API connection and fixed manifest

--- src/backend/openAI.py
+++ src/backend/openAI.py
@@ -1,1 +1,14 @@
 # api to format/send requests to the OpenAI API, and handle responses
+
+from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "Hello, you are testing."},
+    {"role": "user", "content": "Hello, what is your name"}
+  ]
+)
+
+print(completion.choices[0].message)
