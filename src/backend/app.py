f893589f80e956f541e6ecbdd5bd3e671ad3ef33
Samuel Du <samueldu113@gmail.com>
2023-12-09 20:20:37 -0600

    added ability to send request for summary of webpage to OpenAI API

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,41 +1,68 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from openai import OpenAI
+import os
+from scrapeWebpage import get_text
 
 app = Flask(__name__)
 
 CORS(app)
 client = OpenAI()
+SUMMARY_QUERY = "summarize a page with these words: "
+
 
 @app.route("/")
 def hello_world():
     print("Testing")
     return "<p>Hello, World!</p>"
 
-@app.route('/submit', methods=['POST'])
-def submit():
 
-    print("Testing")
-
+@app.route("/summary", methods=["POST"])
+def summary():
     try:
         data = request.json
-        user_input = data['data']
+        url = data.get("url")
+        if url:
+            print("Received URL:", url)
+            page_text = get_text(url)
+            print("Top 150 words:", page_text)
+            completion = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "user",
+                        "content": SUMMARY_QUERY + page_text,
+                    },
+                ],
+                max_tokens=50,
+            )
+            response = completion.choices[0].message
+            print(f"GPT 3.5 Response {response.content}")
+            return jsonify({"summary": response.content})
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)})
 
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    try:
+        data = request.json
+        user_input = data["data"]
         completion = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "user", "content": user_input},
             ],
-            max_tokens=50
+            max_tokens=50,
         )
-
         result2 = completion.choices[0].message
         print(result2)
-        return jsonify({'summary': result2.content})
-
+        return jsonify({"summary": result2.content})
     except Exception as e:
         print(e)
-        return jsonify({'error': str(e)})
+        return jsonify({"error": str(e)})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=5000, debug=True)  # Set debug=False in production
--- src/backend/scrapeWebpage.py
+++ src/backend/scrapeWebpage.py
@@ -1,18 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
-### Cited source: https://pytutorial.com/how-to-get-text-method-beautifulsoup/, Author: Alexander Williams
-def get_text_url(url):
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        text = soup.get_text(separator='\n', strip=True)
-        return text
-    else:
-        return "Failed to retrieve text from webpage. Webscraping could be blocked." 
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+from collections import Counter
 
-website = input("Enter the website to be summarized: ")
-text = get_text_url(website)
-with open('output.txt', 'w') as f:
-    f.writelines(text)
+nltk.download("punkt")
+nltk.download("stopwords")
+
+
+### Cited source: https://pytutorial.com/how-to-get-text-method-beautifulsoup/, Author: Alexander Williams
+def get_text(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        text = soup.get_text(separator="\n", strip=True)
+        return extract_important_words(text)
+    else:
+        return ""
+
+
+def extract_important_words(text, top_n=50):
+    words = word_tokenize(text)
+    filtered_words = [
+        word for word in words if word.lower() not in stopwords.words("english")
+    ]
+    word_counts = Counter(filtered_words)
+    corpus = [text]
+
+    vectorizer = TfidfVectorizer(stop_words=stopwords.words("english"))
+    tfidf_matrix = vectorizer.fit_transform(corpus)
+    feature_names = vectorizer.get_feature_names_out()
+    tfidf_scores = pd.Series(tfidf_matrix.toarray().flatten(), index=feature_names)
+
+    combined_scores = pd.DataFrame(
+        {"frequency": word_counts, "tfidf": tfidf_scores}
+    ).fillna(0)
+    combined_scores["combined_score"] = (
+        combined_scores["frequency"] * combined_scores["tfidf"]
+    )
+    top_words = combined_scores.sort_values(by="combined_score", ascending=False).head(
+        top_n
+    )
+
+    return str(top_words.index.tolist())
