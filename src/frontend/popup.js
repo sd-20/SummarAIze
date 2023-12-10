de25f82f22cc23c81c35eef004291becf574736a
Samuel Du <samueldu113@gmail.com>
2023-12-09 19:27:47 -0600

    added logic to send url to the /summary endpoint when extension is opened

--- src/frontend/popup.js
+++ src/frontend/popup.js
@@ -1,9 +1,29 @@
+// send url to /summary when the extension is opened
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var activeTab = tabs[0];
+    var activeTabUrl = activeTab.url;
+
+    fetch('http://127.0.0.1:5000/summary', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ url: activeTabUrl }),
+    })
+      .then(response => response.text())
+      .then(data => console.log("Response from server:", data))
+      .catch(error => console.error('Fetch error:', error));
+  });
+});
+
+// send user query to /submit when submit button is clicked
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('submitButton').addEventListener('click', () => {
     let userInput = document.getElementById('userInput').value;
 
     console.log(userInput);
-    
+
     fetch('http://127.0.0.1:5000/submit', {
       method: 'POST',
       headers: {
@@ -12,11 +32,11 @@ document.addEventListener('DOMContentLoaded', function () {
       body: JSON.stringify({ data: userInput }),
       mode: 'cors'
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      document.getElementById('response').innerText = data.summary;
-    })
-    .catch(error => console.error('Error:', error));
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        document.getElementById('response').innerText = data.summary;
+      })
+      .catch(error => console.error('Error:', error));
   });
 });
