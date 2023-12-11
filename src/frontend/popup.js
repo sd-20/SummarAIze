94bbf0d9426c171229458f40add3189ebb745da0
Samuel Du <samueldu113@gmail.com>
2023-12-10 22:49:15 -0600

    added chat UI, loader while waiting for summary from OpenAI API

--- src/frontend/popup.js
+++ src/frontend/popup.js
@@ -10,33 +10,79 @@ document.addEventListener('DOMContentLoaded', function () {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ url: activeTabUrl }),
+      mode: 'cors'
     })
-      .then(response => response.text())
-      .then(data => console.log("Response from server:", data))
-      .catch(error => console.error('Fetch error:', error));
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log("Response from server:", data)
+        document.getElementById('summary').innerText = data.summary;
+        document.querySelector('.loader').style.display = 'none';
+
+      })
+      .catch(error => {
+        console.error('Fetch error:', error)
+        document.querySelector('.loader').style.display = 'none';
+      });
   });
 });
 
-// send user query to /submit when submit button is clicked
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('submitButton').addEventListener('click', () => {
-    let userInput = document.getElementById('userInput').value;
+// append question to chat 
+function appendQuestion(message) {
+  var newMessageDiv = document.createElement('div');
+  newMessageDiv.classList.add('questionDiv');
+  var chatDiv = document.getElementById('chat');
+  newMessageDiv.innerText = message;
+  chatDiv.appendChild(newMessageDiv);
+}
 
-    console.log(userInput);
+// append response to chat  
+function appendResponse(message) {
+  var newMessageDiv = document.createElement('div');
+  newMessageDiv.classList.add('responseDiv');
+  var chatDiv = document.getElementById('chat');
+  newMessageDiv.innerText = message;
+  chatDiv.appendChild(newMessageDiv);
+}
 
-    fetch('http://127.0.0.1:5000/submit', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ data: userInput }),
-      mode: 'cors'
+ERROR_MESSAGE = "Error: failed to fetch response"
+function handleSubmit() {
+  let userInput = document.getElementById('userInput').value;
+
+  console.log(userInput);
+
+  fetch('http://127.0.0.1:5000/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ data: userInput }),
+    mode: 'cors'
+  })
+    .then(response => {
+      return response.json()
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        document.getElementById('response').innerText = data.summary;
-      })
-      .catch(error => console.error('Error:', error));
+    .then(data => {
+      console.log(data)
+      document.getElementById('userInput').value = '';
+      appendQuestion(userInput)
+      appendResponse(data.response)
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error)
+      appendQuestion(ERROR_MESSAGE + error)
+    });
+};
+
+// send user query to /submit when submit button is clicked or 'enter' is pressed
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('submitButton').addEventListener('click', handleSubmit);
+  document.getElementById('userInput').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      handleSubmit();
+    }
   });
 });
